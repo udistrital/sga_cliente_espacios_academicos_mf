93a6258a5f5e77933d1e2f41eb949abcd123cba9
andres limas <limaszandresfelipe@gmail.com>
2024-02-07 12:54:19 -0500

    feat: udistrital/sga_documentacion#38 Creación del micro cliente de espacios academicos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, provideRouter } from '@angular/router';
+import { getSingleSpaExtraProviders } from 'single-spa-angular';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { APP_BASE_HREF } from '@angular/common';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    provideRouter(routes),
+    { provide: APP_BASE_HREF, useValue: '/espacios-academicos/' },
+    getSingleSpaExtraProviders(),
+    provideHttpClient(withFetch()) 
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'sga-espacios-academicos-mf',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
